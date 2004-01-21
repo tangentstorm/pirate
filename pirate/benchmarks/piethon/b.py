fb9c1ecee5101ac889bace8ab7fc95889f57bb55
michal <michal>
2004-01-21 19:33:00 +0000

    pie-thon benchmarks

---
+++ pirate/benchmarks/piethon/b.py
@@ -0,0 +1,29 @@
+import b0
+import b1
+import b2
+import b3
+import b4
+import b5
+import b6
+
+def main():
+    for i in range(2):
+        print "--> iteration", i
+        print "--> b0"
+        b0.main()
+        print "--> b1"
+        b1.main()
+        print "--> b2"
+        b2.main()
+        print "--> b3"
+        b3.main()
+        print "--> b4"
+        b4.main()
+        print "--> b5"
+        b5.main()
+        print "--> b6"
+        b6.main()
+    print "--> All done."
+
+if __name__ == '__main__':
+    main()
