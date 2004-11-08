feb8a72948a1327007b1fcbb7a335ce61de323d6
rubys <rubys>
2004-11-08 02:28:31 +0000

    More tests

--- pirate/test/python/int.py
+++ pirate/test/python/int.py
@@ -1,5 +1,6 @@
 a = 31
 b = -13
+c = 2
 
 print "__abs__", a.__abs__(), abs(a)
 print "__add__", a.__add__(b), a + b
@@ -31,24 +32,24 @@ print "__oct__", a.__oct__(), oct(a)
 print "__or__", a.__or__(b), a | b
 print "__pos__", a.__pos__(), +a
 print "__pow__", a.__pow__(2), a ** 2
-# print "__radd__", a.__radd__(b), b + a
-# print "__rand__", a.__rand__(b), b & a
-# print "__rdiv__", a.__rdiv__(b), b / a
+print "__radd__", a.__radd__(b), b + a
+print "__rand__", a.__rand__(b), b & a
+print "__rdiv__", a.__rdiv__(b), b / a
 # print "__rdivmod__", a.__rdivmod__(b), divmod(b, a)
 # print "__reduce__", a.__reduce__(b)
 # print "__reduce_ex__", a.__reduce_ex__(b)
 print "__repr__", a.__repr__()
-# print "__rfloordiv__", a.__rfloordiv__(b), b // a
-# print "__rlshift__", 2 .__rlshift__(a), a << 2
-# print "__rmod__", a.__rmod__(b),  b % a
-# print "__rmul__", a.__rmul__(b), b * a
-# print "__ror__", a.__ror__(b), b | a
-# print "__rpow__", 2 .__rpow__(a), a ** 2
-# print "__rrshift__", 2 .__rrshift__(a), a >> 2
+print "__rfloordiv__", a.__rfloordiv__(b), b // a
+print "__rlshift__", c.__rlshift__(a), a << 2
+print "__rmod__", a.__rmod__(b),  b % a
+print "__rmul__", a.__rmul__(b), b * a
+print "__ror__", a.__ror__(b), b | a
+# print "__rpow__", c.__rpow__(a), a ** 2
+print "__rrshift__", c.__rrshift__(a), a >> 2
 print "__rshift__", a.__rshift__(2), a >> 2
-# print "__rsub__", a.__rsub__(b), b - a
+print "__rsub__", a.__rsub__(b), b - a
 # print "__rtruediv__", a.__rtruediv__(b)
-# print "__rxor__", a.__rxor__(b), b ^ a
+print "__rxor__", a.__rxor__(b), b ^ a
 # print "__setattr__", a.__setattr__(b)
 print "__str__", a.__str__(), a
 print "__sub__", a.__sub__(b), a - b
